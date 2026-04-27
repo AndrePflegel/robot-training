f3b25af4a0d8112f19b7584aac03d66eddd0b012
AndrePflegel!git config --global user.name AndrePflegelgit config --global user.name AndrePflegel! <andre@pflegel.de>
2026-04-27 19:31:35 +0200

    chore: reduce motor debug output

--- modules/motor_control.py
+++ modules/motor_control.py
@@ -1,11 +1,8 @@
-# modules/motor_control.py
+last_action = None
+
 
 def execute_action(action):
-    """
-    Wandelt abstrakte Actions in Motorbefehle um.
-    Aktuell nur Debug-Ausgabe.
-    Später echte Hardware-Ansteuerung.
-    """
+    global last_action
 
     left_speed = 0
     right_speed = 0
@@ -13,54 +10,39 @@ def execute_action(action):
     if action == "DRIVE_FORWARD_SLOW":
         left_speed = 0.4
         right_speed = 0.4
-
     elif action == "DRIVE_FORWARD_VERY_SLOW":
         left_speed = 0.2
         right_speed = 0.2
-
     elif action == "TURN_LEFT":
         left_speed = -0.5
         right_speed = 0.5
-
     elif action == "TURN_RIGHT":
         left_speed = 0.5
         right_speed = -0.5
-
     elif action == "CURVE_LEFT":
         left_speed = 0.3
         right_speed = 0.6
-
     elif action == "CURVE_RIGHT":
         left_speed = 0.6
         right_speed = 0.3
-
     elif action == "CURVE_LEFT_AWAY_FROM_BOUNDARY":
         left_speed = 0.2
         right_speed = 0.7
-
     elif action == "CURVE_RIGHT_AWAY_FROM_BOUNDARY":
         left_speed = 0.7
         right_speed = 0.2
-
     elif action == "TURN_AROUND":
         left_speed = -0.6
         right_speed = 0.6
-
     elif action == "ESCAPE_CORNER":
         left_speed = -0.4
         right_speed = -0.4
-
     elif action == "SEARCH_TARGET_SLOW_TURN":
         left_speed = -0.2
         right_speed = 0.3
 
-    else:
-        left_speed = 0
-        right_speed = 0
+    if action != last_action:
+        print(f"[MOTOR] action={action} | L={left_speed:.2f} R={right_speed:.2f}")
+        last_action = action
 
-    # DEBUG (wichtig zum Testen!)
-    print(f"[MOTOR] action={action} | L={left_speed:.2f} R={right_speed:.2f}")
-
-    # TODO:
-    # hier später echte Motorsteuerung einbauen
-    # set_motor_speeds(left_speed, right_speed)
+    return left_speed, right_speed
